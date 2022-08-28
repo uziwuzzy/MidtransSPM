111237471ca7925aafe7839fe150f46a45541722
Muhammad Fauzi Masykur <>
2022-08-28 12:14:14 +0700

    fix checksum

--- Package.swift
+++ Package.swift
@@ -8,11 +8,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MidtransCorekit",
-            targets: ["MidtransCorekit"]),
+            name: "MidtransCoreKit",
+            targets: ["MidtransCoreKit"]),
         .library(
-            name: "Midtranskit",
-            targets: ["Midtranskit", "MidtransCorekit"]),
+            name: "MidtransKit",
+            targets: ["MidtransKit", "MidtransCoreKit"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,22 +21,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        //        .binaryTarget(
-        //            name: "MidtransCorekit",
-        //            path: "XCFramework/MidtransCoreKit.xcframework"
-        //        ),
-        //        .binaryTarget(
-        //            name: "Midtranskit",
-        //            path: "XCFramework/MidtransKit.xcframework"
-        //        )
-        .binaryTarget(
-            name: "MidtransCorekit",
-            url: "https://github.com/uziwuzzy/Midtrans-xcf/releases/download/1.22.0/MidtransCoreKit.xcframework-1.22.0.zip",
-            checksum: "055e0ed6f2fe165e893f5c74015d02a1d43e57e1d3c1a29f6f2d02aa534375cc"),
-        .binaryTarget(
-            name: "Midtranskit",
-            url: "https://github.com/uziwuzzy/Midtrans-xcf/releases/download/1.22.0/MidtransKit.xcframework-1.22.0.zip",
-            checksum: "4ea2510f26e45f6d3217029c7b64266d806c21336fca8a49cec3330af98b53a7")
+//        .binaryTarget(
+//            name: "MidtransCoreKit",
+//            path: "XCFramework/MidtransCoreKit.xcframework"
+//        ),
+        .binaryTarget(name: "MidtransCoreKit", url: "https://github.com/uziwuzzy/Midtrans-xcf/releases/download/1.22.0/MidtransCoreKit-1.22.0.zip", checksum: "f2bebf1d8ef9ec9450715ddfe71534a4ecb7008e0c2b8a2362cef6832a01636c"),
+        .binaryTarget(name: "MidtransKit", url: "https://github.com/uziwuzzy/Midtrans-xcf/releases/download/1.22.0/MidtransKit-1.22.0.zip", checksum: "ad04d963dd718b69c1b57e5b00fb85e4bb6f21bb437395fcef02f04af6d747ed")
+//        .binaryTarget(
+//            name: "MidtransKit",
+//            path: "XCFramework/MidtransKit.xcframework"
+//        )
     ]
-    
 )
