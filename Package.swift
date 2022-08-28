36a53a0585e6563216bec9558f135cab181eb453
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-08-28 07:20:40 +0700

    change using remote url

--- Package.swift
+++ Package.swift
@@ -21,14 +21,22 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        //        .binaryTarget(
+        //            name: "MidtransCorekit",
+        //            path: "XCFramework/MidtransCoreKit.xcframework"
+        //        ),
+        //        .binaryTarget(
+        //            name: "Midtranskit",
+        //            path: "XCFramework/MidtransKit.xcframework"
+        //        )
         .binaryTarget(
             name: "MidtransCorekit",
-            path: "XCFramework/MidtransCoreKit.xcframework"
-        ),
+            url: "https://github.com/uziwuzzy/Midtrans-xcf/releases/download/1.22.0/MidtransCoreKit.xcframework-1.22.0.zip",
+            checksum: "055e0ed6f2fe165e893f5c74015d02a1d43e57e1d3c1a29f6f2d02aa534375cc"),
         .binaryTarget(
             name: "Midtranskit",
-            path: "XCFramework/MidtransKit.xcframework"
-        )
+            url: "https://github.com/uziwuzzy/Midtrans-xcf/releases/download/1.22.0/MidtransKit.xcframework-1.22.0.zip",
+            checksum: "4ea2510f26e45f6d3217029c7b64266d806c21336fca8a49cec3330af98b53a7")
     ]
     
 )
