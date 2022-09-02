e0a32e166ee16dd85e4c32fb572466394cd3a6e1
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-09-02 16:06:51 +0700

    update 1.23.0

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Midtrans",
+    defaultLocalization: "en",
+        platforms: [
+            .iOS(.v9)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,7 +25,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "MidtransCoreKit", url: "https://github.com/veritrans/Veritrans-ios-sdk/releases/download/1.23.0/MidtransCoreKit.xcframework-1.23.0.zip", checksum: "131f01b1c3292da27656dcfb176bc52c6f72eceb58cc7cdfbee52edaffb3924b"),
-        .binaryTarget(name: "MidtransKit", url: "https://github.com/veritrans/Veritrans-ios-sdk/releases/download/1.23.0/MidtransKit.xcframework-1.23.0.zip", checksum: "1f4e9339da762a47b71ac8dcd6b719ca6076bb5ad529732772786e421ab0b8b3")
+        .binaryTarget(name: "MidtransCoreKit", url: "https://github.com/uziwuzzy/MidtransSPM/releases/download/1.23.0/MidtransCoreKit.xcframework-1.23.0.zip", checksum: "131f01b1c3292da27656dcfb176bc52c6f72eceb58cc7cdfbee52edaffb3924b"),
+        .binaryTarget(name: "MidtransKit", url: "https://github.com/uziwuzzy/MidtransSPM/releases/download/1.23.0/MidtransKit.xcframework-1.23.0.zip", checksum: "1f4e9339da762a47b71ac8dcd6b719ca6076bb5ad529732772786e421ab0b8b3")
     ]
 )
